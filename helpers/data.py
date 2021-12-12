f7e0a6d77dbf1b381d9029e8772c0a0978269b22
Ludwig Stumpp <ludwig.stumpp@unternehmertum.de>
2021-12-12 11:53:47 +0100

    Update data functions

--- helpers/data.py
+++ helpers/data.py
@@ -7,43 +7,54 @@ from sklearn.datasets import make_swiss_roll
 # collection of functions to generate datasets
 
 
-def _get_unit_cycle_data(k: int, N: int) -> np.ndarray:
-    """
-    Helper function to generate uniformly distributed points on the unit circle.
+def _get_unit_cycle_data_k(k: int, N: int) -> tuple[np.ndarray, np.ndarray]:
+    """Helper function to generate uniformly distributed points on the unit circle.
+
+    Calculates the k-th term of the sequence of points on the unit circle as:
+    t = (2 * np.pi * k) / (N + 1)
+    x = (cos(t), sin(t))
 
     Args:
         k (int): Current index of the point to generate.
         N (int): Total number of points to generate.
 
     Returns:
-        np.ndarray: Point on the unit circle, in the form of a 2D array of shape (2,).
+        tuple[np.ndarray, np.ndarray]: Tuple of t and x.
     """
-    t = 2 * math.pi * k / (N + 1)
+    t = np.array(2 * math.pi * k / (N + 1))
     x = np.array([np.cos(t), np.sin(t)])
-    return x
+    return x, t
 
 
-def get_unit_cycle_dataset(N: int) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Generate a dataset of N points, uniformly distributed on the unit circle.
+def get_unit_cycle_dataset(N: int) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Generate a dataset of N points, uniformly distributed on the unit circle.
+
+    The data is defined as the set of all 2D points x in R^2, for k = 1, ..., N, as:
+        x = (cos(t), sin(t))
+        t = (2 * np.pi * k) / (N + 1)
 
     Args:
         N (int): Number of points in the dataset to generate uniformly distributed on the unit circle.
 
     Returns:
-        tuple[np.ndarray, np.ndarray]: Tuple of data and colors.
+        tuple[np.ndarray, np.ndarray, np.ndarray]: Tuple of data, t, and colors.
             Data is a 2D array of shape (N, 2), where each row is a point in the dataset.
             Colors is a 1D array of shape (N,), where each element is the color of the corresponding point.
     """
     k_vec = np.arange(1, N + 1)
     colors = k_vec
-    X = _get_unit_cycle_data(k_vec, len(k_vec))
-    return X.T, colors
+    x, t  = _get_unit_cycle_data_k(k_vec, len(k_vec))
+    return x.T, t, colors
 
 
 def get_swiss_roll_dataset(N: int) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Get swiss roll dataset of N points.
+    """Get swiss roll dataset of N points.
+
+    Getting dataset from https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_swiss_roll.html.
+
+    The data is defined as the set of all 3D points x in R^3, for k = 1, ..., N, as:
+        x = (u*cos(u), v, u*sin(u))
+        where (u, v) in [0, 10]^2 are chosen uniformly at random.
 
     Args:
         N (int): N points in the dataset to generate.
@@ -54,8 +65,8 @@ def get_swiss_roll_dataset(N: int) -> tuple[np.ndarray, np.ndarray]:
             Colors is a 1D array of shape (N,), where each element is the color of the corresponding point.
     """
 
-    X, color = make_swiss_roll(n_samples=N)
-    return X, color
+    x, color = make_swiss_roll(n_samples=N)
+    return x, color
 
 
 def get_trajectory_dataset(path: str = "data/data_DMAP_PCA_vadere.txt") -> np.ndarray:
